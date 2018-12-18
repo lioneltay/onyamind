3fb291cadcec4c29a30b76bc59a822687931e4ed
Lionel Tay <lionel.lt.tay@gmail.com>
2018-12-18 23:23:48 +1100

    minor change to trigger build

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -9,7 +9,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <Container>
-        <h1>Home!</h1>
+        <h1>Home! - Travis CI</h1>
 
         <Link to="/sandbox">Sandbox</Link>
       </Container>
