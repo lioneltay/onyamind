b313b4460d8ca2fd866e9d55b07ceb69c7613f49
Lionel Tay <lionel.lt.tay@gmail.com>
2018-12-24 22:27:56 +1100

    remove connection to local postgresql server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import { createConnection } from "typeorm"
+// import { createConnection } from "typeorm"
 import Express = require("express")
 import * as bodyParser from "body-parser"
 import * as cors from 'cors'
@@ -8,7 +8,7 @@ import applyRoutes from "routes"
 const app = Express()
 
 async function init() {
-  await createConnection()
+  // await createConnection()
 
   app.use(cors())
   app.use(bodyParser.json())
