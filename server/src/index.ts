0f3f1d363b08a14b135a8ce712b015c65e23db11
Lionel Tay <lionel.lt.tay@gmail.com>
2018-12-24 22:38:03 +1100

    Change from F1 -> F2 instance for server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,21 @@
-// import { createConnection } from "typeorm"
 import Express = require("express")
 import * as bodyParser from "body-parser"
-import * as cors from 'cors'
+import * as cors from "cors"
 
 import applyRoutes from "routes"
 
 const app = Express()
 
-async function init() {
-  // await createConnection()
+app.use(cors())
+app.use(bodyParser.json())
 
-  app.use(cors())
-  app.use(bodyParser.json())
+applyRoutes(app)
 
-  applyRoutes(app)
+app.get("*", (req, res) => {
+  res.send("Didn't hit a route this time :/")
+})
 
-  app.get("*", (req, res) => {
-    res.send("Didn't hit a route this time :/")
-  })
-
-  const PORT = process.env.PORT || 3030
-  app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`)
-  })
-}
-
-init()
+const PORT = process.env.PORT || 3030
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`)
+})
