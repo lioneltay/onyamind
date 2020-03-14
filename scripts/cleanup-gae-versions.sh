ab8299830c7874322a22dfb35ed83ca961fa4ad0
Lionel Tay <lionel.tay@outlook.com>
2020-03-14 14:20:03 +1100

    install gcloud on travis

--- scripts/cleanup-gae-versions.sh
+++ scripts/cleanup-gae-versions.sh
@@ -1,9 +1,10 @@
-VERSIONS=$(gcloud app versions list --service client-dev --sort-by '~versions' --format 'value(version.id)' | sort -r | tail -n +3 | paste -sd " " -)
+SERVICE=default
+# +1 accounts for tail behaviour
+VERSIONS_TO_KEEP=$((3 + 1))
+VERSIONS=$(gcloud app versions list --service $SERVICE --sort-by '~versions' --format 'value(version.id)' | sort -r | tail -n +$VERSIONS_TO_KEEP | paste -sd " " -)
 
-gcloud app versions list --service client-dev
-
-# if [ ${#VERSIONS} -gt 0 ]
-# then
-#     # If you want to confirm before deletion, remove the -q
-#     gcloud app versions delete --service client-dev $(echo $VERSIONS) -q
-# fi
+if [ ${#VERSIONS} -gt 0 ]
+then
+    # If you want to confirm before deletion, remove the -q
+    gcloud app versions delete --service $SERVICE $(echo $VERSIONS) -q
+fi
