6d774807bc45582c8ea969484d1b69ca13c8ecef
Lionel Tay <lionel.tay@outlook.com>
2020-03-17 20:59:26 +1100

    notification test

--- src/services/notifications/index.ts
+++ src/services/notifications/index.ts
@@ -1,11 +1,14 @@
 export const createTaskNotification = async (task: Task) => {
   if (!Notification.permission) {
+    console.log('REQUESTIONG PERMISSION')
     await Notification.requestPermission()
   }
 
   new Notification(task.title, {
     requireInteraction: true,
     body: task.notes,
+    renotify: true,
+    tag: task.id,
   })
 }
 
