5642b876f4c7832e39269b4ed9e2869c3cbf7d40
Lionel Tay <lionel.tay@outlook.com>
2020-03-22 15:02:34 +1100

    don't close drawer from list page useEffect

--- cypress/e2e/my_tests.spec.ts
+++ cypress/e2e/my_tests.spec.ts
@@ -1,5 +1,8 @@
 describe("Intialization", function() {
   it("Initialize the anonymous user and create first 'Todo' list", () => {
-    cy.visit("/").findByText(/todo/i)
+    cy.visit("/").get(
+      ".HeaderBase___StyledDiv2-sc-175njhs-2 > .MuiButtonBase-root > .MuiIconButton-label > .MuiSvgIcon-root > path",
+    )
+    .click()
   })
 })
--- src/pages/lists/Page.tsx
+++ src/pages/lists/Page.tsx
@@ -176,7 +176,6 @@ export default ({
       } else {
         selectPrimaryTaskList()
       }
-      closeDrawer()
     }
   }, [taskListsLoaded, listIdParamValid])
 
