3adb65eb82d4054e55625ba526e70f405818d26f
Lionel Tay <lionel.tay@outlook.com>
2020-03-28 20:00:44 +1100

    add cypress helper, split tests

--- cypress/utils/index.js
+++ cypress/utils/index.js
@@ -6,3 +6,13 @@ export const resetDB = () => {
     "http://localhost:8080/emulator/v1/projects/onyamind-staging/databases/(default)/documents",
   )
 }
+
+export function visitListPage(listInfo) {
+  if (listInfo && listInfo.id) {
+    cy.visit(`lists/${listInfo.id}`)
+    cy.findByText(new RegExp(`${listInfo.name}`, "i"))
+  } else {
+    cy.visit("/lists")
+    cy.findByText(/todo/i)
+  }
+}
