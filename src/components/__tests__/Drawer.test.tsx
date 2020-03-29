9e1c06153c809e7f101f929a9ccb098c6e15f812
Lionel Tay <lionel.tay@outlook.com>
2020-03-29 14:04:04 +1100

    wokring

---
+++ src/components/__tests__/Drawer.test.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { renderWithWrappers } from "testing"
+import user from "@testing-library/react"
+import Drawer from "../Drawer"
+
+describe("<Drawer />", () => {
+  test("FeedbackModal allows user to submit feedback", () => {
+    expect(5).toBe(5)
+  })
+})
