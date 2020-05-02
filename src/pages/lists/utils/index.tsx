30eb83dd7a5c454f00bffb25f15e5aee67f71be9
Lionel Tay <lionel.tay@outlook.com>
2020-05-02 17:58:24 +1000

    only reset routines

--- src/pages/lists/utils/index.tsx
+++ src/pages/lists/utils/index.tsx
@@ -63,6 +63,7 @@ export const useHandleRoutineReset = () => {
 
       if (
         !list ||
+        !list.routine ||
         (list.routineRestartedAt &&
           new Date(list.routineRestartedAt) > getRoutineResetTime())
       ) {
