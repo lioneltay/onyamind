b6d86a85ce3fc997639e7a119828b80787804797
Lionel Tay <lionel.tay@outlook.com>
2020-05-02 18:16:43 +1000

    add taskOrder sync

--- functions/src/functions/sync.ts
+++ functions/src/functions/sync.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions"
 import { admin } from "../utils/firebase"
+import * as firebase from "firebase-admin"
 
 /**
  * Keep taskList numberOfCompleteTasks and numberOfIncompleteTasks up to date as tasks are changed
@@ -45,6 +46,53 @@ export const syncListTaskCount = functions.firestore
     // Delete - do nothing
     if (before && !after) {
       return
+    }
+
+    return
+  })
+
+/**
+ * Keep list.taskOrder in sync when moving/deleting tasks
+ */
+export const syncListTaskOrder = functions.firestore
+  .document("/taskList/{listId}")
+  .onWrite(async (change, context) => {
+    const before = change.before.data() as Task | undefined
+    const after = change.after.data() as Task | undefined
+
+    type UpdateInput = {
+      taskId: ID
+      listId: ID
+    }
+    function removeTaskFromOrder({ taskId, listId }: UpdateInput) {
+      return admin
+        .firestore()
+        .collection("taskList")
+        .doc(listId)
+        .update({
+          taskOrder: firebase.firestore.FieldValue.arrayRemove(taskId),
+        })
+    }
+
+    // Update
+    if (before && after) {
+      // Handle moving a task
+      if (before.listId !== after.listId) {
+        return removeTaskFromOrder({ taskId: before.id, listId: before.listId })
+      }
+
+      return
+    }
+
+    // Create
+    if (!before && after) {
+      // do nothing
+      return
+    }
+
+    // Delete
+    if (before && !after) {
+      return removeTaskFromOrder({ taskId: before.id, listId: before.listId })
     }
 
     return
